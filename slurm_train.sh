490a42b115fa8de88cd2f03c813e602e6814422d
ZhenahangHuang <prc_hzh@163.com>
2023-05-17 12:09:37 +0800

    update

--- slurm_train.sh
+++ slurm_train.sh
@@ -1,43 +1,37 @@
-#!/bin/bash
+set -x
 
-# Change these!
-#SBATCH --partition=<partition_name>
-#SBATCH --job-name=<job_name>
-#SBATCH --nodes 1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=6
-#SBATCH --output=%x_%j.out
-#SBATCH --error=%x_%j.err
-#SBATCH --comment=<comment>
-#SBATCH --open-mode=append
-#SBATCH --requeue
+PARTITION=$1
+JOB_NAME=$2
+GPUS=${GPUS:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-16}
+SRUN_ARGS=${SRUN_ARGS:-""}
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
+MASTER_ADDR="0.0.0.0"
+MASTER_PORT=12802
+# export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 
-# Example usage:
-# sbatch slurm_train.sh
-# Run using conda and make sure to have the conda env activated when running sbatch.
-
-
-module load openmpi
-export PYTHONFAULTHANDLER=1
-export CUDA_LAUNCH_BLOCKING=0
-export HOSTNAMES=`scontrol show hostnames "$SLURM_JOB_NODELIST"`
-export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_PORT=12802
-export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
-echo go $COUNT_NODE
-echo $HOSTNAMES
-
-# Change these as needed!
-DATA_PATH="/path/to/data/dir"
+# DATA_PATH="/mnt/petrelfs/huangzhenhang/datasets/datacomp/shards"
+DATA_PATH="./out/laion2b/"
 SCALE="small"
 SEED=0
-OUTPUT_DIR="/path/to/output/dir"
+OUTPUT_DIR="./out/"
 NUM_CHECKPOINTS=8
-EXP_NAME="datacomp-scale-${SCALE}-seed${SEED}"
+EXP_NAME="datacomp-scale-${SCALE}-seed${SEED}_basic-filter"
 PRECISION="amp"  # You can also use amp_bfloat16 if supported by your hardware.
 
 # Change comment as needed
-srun --comment "<comment>" --cpu_bind=v --accel-bind=gn python train.py \
+srun \
+    --partition=${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --kill-on-bad-exit=1 \
+    --quotatype=reserved \
+    ${SRUN_ARGS} \
+    python train.py \
     --scale ${SCALE} \
     --data_dir ${DATA_PATH} \
     --output_dir ${OUTPUT_DIR} \
@@ -47,4 +41,6 @@ srun --comment "<comment>" --cpu_bind=v --accel-bind=gn python train.py \
     --seed ${SEED} \
     --report_to_wandb \
     --dataset_resampled \
-    --accum_freq 1 
+    --accum_freq 1 ${@:4}
+    
+    # --cpu_bind=v --accel-bind=gn \
